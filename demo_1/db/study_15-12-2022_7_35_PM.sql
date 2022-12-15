2643c75724d44a8cf6f3582b4aede24ba7c478c8
Altrinjazz <118260601+Altrinjazz@users.noreply.github.com>
2022-12-15 19:37:18 +0530

    data base

---
+++ demo_1/db/study_15-12-2022_7_35_PM.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2022 at 03:01 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo_dp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study`
+--
+
+CREATE TABLE `study` (
+  `id` int(25) NOT NULL,
+  `name` varchar(43) NOT NULL,
+  `gmail` varchar(32) NOT NULL,
+  `pass` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `study`
+--
+
+INSERT INTO `study` (`id`, `name`, `gmail`, `pass`) VALUES
+(1, '35', '53', '76'),
+(2, 'Ram', 'ram@gmail.com', '123476');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `study`
+--
+ALTER TABLE `study`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `study`
+--
+ALTER TABLE `study`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
